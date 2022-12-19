f02aa9d3859a1bff2a081937531def8ffb59664e
Jeffrey <97353049+Delacrobix@users.noreply.github.com>
2022-12-19 16:29:12 -0500

    version pre-alpha

---
+++ nodejs/src/app.js
@@ -0,0 +1,46 @@
+/**
+ * *Cuerpo de la aplicación:
+ * *Aplicación construida con express y nodejs, mongodb en la  base de datos y
+ * *pug como motor de plantillas.
+ */
+
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const session = require('express-session');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const MONGODB = process.env.MONGODB,
+      PORT = process.env.PORT;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.set('view engine', 'pug');
+app.use(methodOverride());
+app.use(express.static(__dirname + '/static'));
+app.set('views', __dirname + '/views');
+
+app.use(session({
+    secret: 'sec',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.listen(PORT, () =>{
+    console.log(`Server running on: ${PORT}`);
+});
+
+/**
+ * *Importación de las rutas.
+ */
+app.use(require('./routes/routes'));
+
+/**
+ * *Conexión a la base de datos.
+ */
+mongoose.connect(MONGODB, {})
+    .then(db => console.log(`DB is connected to: ${MONGODB}`))
+    .catch(err => console.error(err));
